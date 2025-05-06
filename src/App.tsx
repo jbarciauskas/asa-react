9e91b36c3fc6c76a34809d1874fba1092d0ba7d0
Joel Barciauskas <barciajo@gmail.com>
2025-05-06 14:41:12 -0400

    Move tables into tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import { Box, Typography, Container } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, Container, Tabs, Tab } from '@mui/material';
 import LeagueGoalsAddedTable from './components/LeagueGoalsAddedTable';
 
 function App() {
+  const [selectedLeague, setSelectedLeague] = useState<'nwsl' | 'mls'>('nwsl');
+
   return (
     <Container maxWidth="lg">
       <Box py={4}>
         <Typography variant="h3" component="h1" gutterBottom>
           Soccer Analytics Dashboard
         </Typography>
-        
-        <Box mb={6}>
-          <Typography variant="h4" component="h2" gutterBottom>
-            NWSL G+
-          </Typography>
-          <LeagueGoalsAddedTable league="nwsl" />
+
+        <Box mb={4}>
+          <Tabs
+            value={selectedLeague}
+            onChange={(_e, newValue) => setSelectedLeague(newValue)}
+            aria-label="League Tabs"
+          >
+            <Tab label="NWSL G+" value="nwsl" />
+            <Tab label="MLS G+" value="mls" />
+          </Tabs>
         </Box>
 
-        <Box>
-          <Typography variant="h4" component="h2" gutterBottom>
-            MLS G+
-          </Typography>
-          <LeagueGoalsAddedTable league="mls" />
-        </Box>
+        <LeagueGoalsAddedTable league={selectedLeague} />
       </Box>
     </Container>
   );
